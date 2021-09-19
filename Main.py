1d417460be88b02796fde0174370d7d9a5afa2dd
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-09-19 22:52:16 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,112 @@
+import argparse
+import os
+import numpy as np
+import math
+import itertools
+import sys
+import datetime
+import time
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+from Model import *
+from Dataset import *
+from Train import *
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--dataset_name", type=str, default="edges2shoes", help="name of the dataset")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--img_height", type=int, default=64, help="size of image height")
+parser.add_argument("--img_width", type=int, default=64, help="size of image width")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving generator samples")
+parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between model checkpoints")
+opt = parser.parse_args()
+print(opt)
+
+# Create sample and checkpoint directories
+os.makedirs("images/%s" % opt.dataset_name, exist_ok=True)
+os.makedirs("saved_models/%s" % opt.dataset_name, exist_ok=True)
+
+# Losses
+adversarial_loss = torch.nn.MSELoss()
+cycle_loss = torch.nn.L1Loss()
+pixelwise_loss = torch.nn.L1Loss()
+
+cuda = torch.cuda.is_available()
+
+input_shape = (opt.channels, opt.img_height, opt.img_width)
+
+# Initialize generator and discriminator
+G_AB = GeneratorUNet(input_shape)
+G_BA = GeneratorUNet(input_shape)
+D_A = Discriminator(input_shape)
+D_B = Discriminator(input_shape)
+
+if cuda:
+    G_AB = G_AB.cuda()
+    G_BA = G_BA.cuda()
+    D_A = D_A.cuda()
+    D_B = D_B.cuda()
+    adversarial_loss.cuda()
+    cycle_loss.cuda()
+    pixelwise_loss.cuda()
+
+if opt.epoch != 0:
+    # Load pretrained models
+    G_AB.load_state_dict(torch.load("saved_models/%s/G_AB_%d.pth" % (opt.dataset_name, opt.epoch)))
+    G_BA.load_state_dict(torch.load("saved_models/%s/G_BA_%d.pth" % (opt.dataset_name, opt.epoch)))
+    D_A.load_state_dict(torch.load("saved_models/%s/D_A_%d.pth" % (opt.dataset_name, opt.epoch)))
+    D_B.load_state_dict(torch.load("saved_models/%s/D_B_%d.pth" % (opt.dataset_name, opt.epoch)))
+else:
+    # Initialize weights
+    G_AB.apply(weights_init_normal)
+    G_BA.apply(weights_init_normal)
+    D_A.apply(weights_init_normal)
+    D_B.apply(weights_init_normal)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(
+    itertools.chain(G_AB.parameters(), G_BA.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
+)
+optimizer_D_A = torch.optim.Adam(D_A.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D_B = torch.optim.Adam(D_B.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+# Input tensor type
+Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
+
+# Dataset loader
+transforms_ = [
+    transforms.Resize((opt.img_height, opt.img_width), Image.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+]
+dataloader = DataLoader(
+    ImageDataset("../../data/%s" % opt.dataset_name, transforms_=transforms_, mode="train"),
+    batch_size=opt.batch_size,
+    shuffle=True,
+    num_workers=opt.n_cpu,
+)
+val_dataloader = DataLoader(
+    ImageDataset("../../data/%s" % opt.dataset_name, transforms_=transforms_, mode="val"),
+    batch_size=16,
+    shuffle=True,
+    num_workers=opt.n_cpu,
+)
+
+Train()
